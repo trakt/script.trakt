8c3387454fb35e7d814e843f9fc9e4ddef044a92
Sebastian Schocke <huntrckr@gmail.com>
2013-02-09 13:03:59 +0200

    Sort out indentation

--- rating.py
+++ rating.py
@@ -16,85 +16,85 @@ __status__ = "Production"
 __settings__ = xbmcaddon.Addon( "script.trakt" )
 
 def ratingCheck(current_video, watched_time, total_time, playlist_length):
-    """Check if a video should be rated and if so launches the correct rating window"""
-    settings = xbmcaddon.Addon("script.trakt")
+	"""Check if a video should be rated and if so launches the correct rating window"""
+	settings = xbmcaddon.Addon("script.trakt")
 
-    rate_movies = settings.getSetting("rate_movie")
-    rate_episodes = settings.getSetting("rate_episode")
-    rate_each_playlist_item = settings.getSetting("rate_each_playlist_item")
-    rate_min_view_time = settings.getSetting("rate_min_view_time")
-    
-    Debug("[Rating] Rating Check called for " + current_video['type'] + " id=" + str(current_video['id']) );
+	rate_movies = settings.getSetting("rate_movie")
+	rate_episodes = settings.getSetting("rate_episode")
+	rate_each_playlist_item = settings.getSetting("rate_each_playlist_item")
+	rate_min_view_time = settings.getSetting("rate_min_view_time")
 
-    if (watched_time/total_time)*100>=float(rate_min_view_time):
-        if (playlist_length <= 1) or (rate_each_playlist_item == 'true'):
-            if current_video['type'] == 'movie' and rate_movies == 'true':
-                rateMovie(current_video['id'])
-            if current_video['type'] == 'episode' and rate_episodes == 'true':
-                rateEpisode(current_video['id'])
+	Debug("[Rating] Rating Check called for " + current_video['type'] + " id=" + str(current_video['id']) );
+
+	if (watched_time/total_time)*100>=float(rate_min_view_time):
+		if (playlist_length <= 1) or (rate_each_playlist_item == 'true'):
+			if current_video['type'] == 'movie' and rate_movies == 'true':
+				rateMovie(current_video['id'])
+			if current_video['type'] == 'episode' and rate_episodes == 'true':
+				rateEpisode(current_video['id'])
 
 
 def rateMovie(movieid):
-    """Launches the movie rating dialogue"""
-    if movieid == None:
-        return
-        
-    match = utilities.getMovieDetailsFromXbmc(movieid, ['imdbnumber', 'title', 'year'])
-    if not match:
-        #add error message here
-        return
+	"""Launches the movie rating dialogue"""
+	if movieid == None:
+		return
 
-    imdbid = match['imdbnumber']
-    title = match['title']
-    year = match['year']
+	match = utilities.getMovieDetailsFromXbmc(movieid, ['imdbnumber', 'title', 'year'])
+	if not match:
+		#add error message here
+		return
 
-    Debug("[Rating] Rating movie '" + title + "' (IMDBID:"+str(imdbid)+")" );
-    
-    ratingType = utilities.traktSettings["viewing"]["ratings"]["mode"]
-    curRating = utilities.getMovieRatingFromTrakt(imdbid, title, year, ratingType)
-    if ratingType == "advanced":
-        if curRating != 0:
-            return
-        gui = windows.RateMovieDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
-    else:
-        if curRating != False:
-            return
-        gui = windows.RateMovieDialog("rate.xml", __settings__.getAddonInfo('path'))
+	imdbid = match['imdbnumber']
+	title = match['title']
+	year = match['year']
 
-    gui.initDialog(imdbid, title, year, curRating, ratingType)
-    gui.doModal()
-    del gui
+	Debug("[Rating] Rating movie '" + title + "' (IMDBID:"+str(imdbid)+")" );
+
+	ratingType = utilities.traktSettings["viewing"]["ratings"]["mode"]
+	curRating = utilities.getMovieRatingFromTrakt(imdbid, title, year, ratingType)
+	if ratingType == "advanced":
+		if curRating != 0:
+			return
+		gui = windows.RateMovieDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
+	else:
+		if curRating != False:
+			return
+		gui = windows.RateMovieDialog("rate.xml", __settings__.getAddonInfo('path'))
+
+	gui.initDialog(imdbid, title, year, curRating, ratingType)
+	gui.doModal()
+	del gui
 
 
 def rateEpisode(episode_id):
-    if episode_id == None:
-        return
-        
-    """Launches the episode rating dialogue"""
-    match = utilities.getEpisodeDetailsFromXbmc(episode_id, ['showtitle', 'season', 'episode','tvshowid'])
-    if not match:
-        #add error message here
-        return
+	if episode_id == None:
+		return
 
-    tvdbid = match['tvdb_id']
-    title = match['showtitle']
-    year = match['year']
-    season = match['season']
-    episode = match['episode']
+	"""Launches the episode rating dialogue"""
+	match = utilities.getEpisodeDetailsFromXbmc(episode_id, ['showtitle', 'season', 'episode','tvshowid'])
+	if not match:
+		#add error message here
+		return
 
-    Debug("[Rating] Rating tv episode '" + title + "' "+str(season)+"x"+str(episode)+" (TVDBID:"+str(tvdbid)+")");
-    
-    ratingType = utilities.traktSettings["viewing"]["ratings"]["mode"]
-    curRating = utilities.getEpisodeRatingFromTrakt(tvdbid, title, year, season, episode, ratingType)
-    if ratingType == "advanced":
-        if curRating != 0:
-            return
-        gui = windows.RateEpisodeDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
-    else:
-        if curRating != False:
-            return
-        gui = windows.RateEpisodeDialog("rate.xml", __settings__.getAddonInfo('path'))
+	tvdbid = match['tvdb_id']
+	title = match['showtitle']
+	year = match['year']
+	season = match['season']
+	episode = match['episode']
 
-    gui.initDialog(tvdbid, title, year, season, episode, curRating, ratingType)
-    gui.doModal()
-    del gui
+	Debug("[Rating] Rating tv episode '" + title + "' "+str(season)+"x"+str(episode)+" (TVDBID:"+str(tvdbid)+")");
+
+	ratingType = utilities.traktSettings["viewing"]["ratings"]["mode"]
+	curRating = utilities.getEpisodeRatingFromTrakt(tvdbid, title, year, season, episode, ratingType)
+	if ratingType == "advanced":
+		if curRating != 0:
+			return
+		gui = windows.RateEpisodeDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
+	else:
+		if curRating != False:
+			return
+		gui = windows.RateEpisodeDialog("rate.xml", __settings__.getAddonInfo('path'))
+
+	gui.initDialog(tvdbid, title, year, season, episode, curRating, ratingType)
+	gui.doModal()
+	del gui
--- windows.py
+++ windows.py
@@ -59,155 +59,155 @@ ACTION_CONTEXT_MENU = 117
 
 
 class RateDialog(xbmcgui.WindowXMLDialog):
-    """Base class implementing the methods that don't change in the rating dialogues"""
-    def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
-        xbmcgui.WindowXMLDialog.__init__(xml, fallback_path, defaultskinname, forcefallback)
+	"""Base class implementing the methods that don't change in the rating dialogues"""
+	def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
+		xbmcgui.WindowXMLDialog.__init__(xml, fallback_path, defaultskinname, forcefallback)
 
-        self._id_to_rating_string = {
-            RATE_SKIP_RATING: "skip", RATE_LOVE_BTN: "love", RATE_HATE_BTN: "hate", RATE_ADVANCED_1_BTN: "1",
-            RATE_ADVANCED_2_BTN: "2", RATE_ADVANCED_3_BTN: "3", RATE_ADVANCED_4_BTN: "4",
-            RATE_ADVANCED_5_BTN: "5", RATE_ADVANCED_6_BTN: "6", RATE_ADVANCED_7_BTN: "7",
-            RATE_ADVANCED_8_BTN: "8", RATE_ADVANCED_9_BTN: "9", RATE_ADVANCED_10_BTN: "10"
-        }
+		self._id_to_rating_string = {
+			RATE_SKIP_RATING: "skip", RATE_LOVE_BTN: "love", RATE_HATE_BTN: "hate", RATE_ADVANCED_1_BTN: "1",
+			RATE_ADVANCED_2_BTN: "2", RATE_ADVANCED_3_BTN: "3", RATE_ADVANCED_4_BTN: "4",
+			RATE_ADVANCED_5_BTN: "5", RATE_ADVANCED_6_BTN: "6", RATE_ADVANCED_7_BTN: "7",
+			RATE_ADVANCED_8_BTN: "8", RATE_ADVANCED_9_BTN: "9", RATE_ADVANCED_10_BTN: "10"
+		}
 
-        self.curRating = None
-        self.controlID = None
+		self.curRating = None
+		self.controlID = None
 
-    def initDialog(self, cur_rating, rating_type):
-        """Set up the generic current rating code"""
-        self.curRating = cur_rating
-        self.ratingType = rating_type
-        if self.curRating not in self._id_to_rating_string.values():
-            self.curRating = None
+	def initDialog(self, cur_rating, rating_type):
+		"""Set up the generic current rating code"""
+		self.curRating = cur_rating
+		self.ratingType = rating_type
+		if self.curRating not in self._id_to_rating_string.values():
+			self.curRating = None
 
-    def onFocus(self, control_id):
-        """Update currently selected item id"""
-        self.controlID = control_id
+	def onFocus(self, control_id):
+		"""Update currently selected item id"""
+		self.controlID = control_id
 
-    def onInit(self, header):
-        """Set up the generic window code"""
-        self.getControl(RATE_TITLE).setLabel(header)
-        self.getControl(RATE_RATE_SHOW_BG).setVisible(False)
-        self.getControl(RATE_RATE_SHOW_BTN).setVisible(False)
-        self.getControl(RATE_CUR_NO_RATING).setEnabled(False)
-        self.setFocus(self.getControl(RATE_SKIP_RATING))
-        self._update_rated_button()
+	def onInit(self, header):
+		"""Set up the generic window code"""
+		self.getControl(RATE_TITLE).setLabel(header)
+		self.getControl(RATE_RATE_SHOW_BG).setVisible(False)
+		self.getControl(RATE_RATE_SHOW_BTN).setVisible(False)
+		self.getControl(RATE_CUR_NO_RATING).setEnabled(False)
+		self.setFocus(self.getControl(RATE_SKIP_RATING))
+		self._update_rated_button()
 
-    def onClick(self, control_id, method):
-        """Perform action when item clicked"""
-        self.curRating = self._id_to_rating_string[control_id]
-        self._update_rated_button()
+	def onClick(self, control_id, method):
+		"""Perform action when item clicked"""
+		self.curRating = self._id_to_rating_string[control_id]
+		self._update_rated_button()
 
-        if control_id in (RATE_CUR_LOVE, RATE_CUR_HATE, RATE_CUR_ADVANCED_1, RATE_CUR_ADVANCED_2, RATE_CUR_ADVANCED_3, RATE_CUR_ADVANCED_4, RATE_CUR_ADVANCED_5, RATE_CUR_ADVANCED_6, RATE_CUR_ADVANCED_7, RATE_CUR_ADVANCED_8, RATE_CUR_ADVANCED_9, RATE_CUR_ADVANCED_10):
-            self.curRating = "unrate"
+		if control_id in (RATE_CUR_LOVE, RATE_CUR_HATE, RATE_CUR_ADVANCED_1, RATE_CUR_ADVANCED_2, RATE_CUR_ADVANCED_3, RATE_CUR_ADVANCED_4, RATE_CUR_ADVANCED_5, RATE_CUR_ADVANCED_6, RATE_CUR_ADVANCED_7, RATE_CUR_ADVANCED_8, RATE_CUR_ADVANCED_9, RATE_CUR_ADVANCED_10):
+			self.curRating = "unrate"
 
-        if self.curRating != "skip":
-            method(self.curRating)
+		if self.curRating != "skip":
+			method(self.curRating)
 
-        Debug("[Rating] Closing and rated! "+str(self.curRating))
-        self.close()
+		Debug("[Rating] Closing and rated! "+str(self.curRating))
+		self.close()
 
-    def _update_rated_button(self):
-        """Set the current rating button"""
-        self.getControl(RATE_CUR_NO_RATING).setVisible(False if self.curRating != None else True)
-        self.getControl(RATE_CUR_LOVE).setVisible(False if self.curRating != "love" else True)
-        self.getControl(RATE_CUR_HATE).setVisible(False if self.curRating != "hate" else True)
-        if self.ratingType == "advanced":
-            self.getControl(RATE_CUR_ADVANCED_1).setVisible(False if self.curRating != "1" else True)
-            self.getControl(RATE_CUR_ADVANCED_2).setVisible(False if self.curRating != "2" else True)
-            self.getControl(RATE_CUR_ADVANCED_3).setVisible(False if self.curRating != "3" else True)
-            self.getControl(RATE_CUR_ADVANCED_4).setVisible(False if self.curRating != "4" else True)
-            self.getControl(RATE_CUR_ADVANCED_5).setVisible(False if self.curRating != "5" else True)
-            self.getControl(RATE_CUR_ADVANCED_6).setVisible(False if self.curRating != "6" else True)
-            self.getControl(RATE_CUR_ADVANCED_7).setVisible(False if self.curRating != "7" else True)
-            self.getControl(RATE_CUR_ADVANCED_8).setVisible(False if self.curRating != "8" else True)
-            self.getControl(RATE_CUR_ADVANCED_9).setVisible(False if self.curRating != "9" else True)
-            self.getControl(RATE_CUR_ADVANCED_10).setVisible(False if self.curRating != "10" else True)
+	def _update_rated_button(self):
+		"""Set the current rating button"""
+		self.getControl(RATE_CUR_NO_RATING).setVisible(False if self.curRating != None else True)
+		self.getControl(RATE_CUR_LOVE).setVisible(False if self.curRating != "love" else True)
+		self.getControl(RATE_CUR_HATE).setVisible(False if self.curRating != "hate" else True)
+		if self.ratingType == "advanced":
+			self.getControl(RATE_CUR_ADVANCED_1).setVisible(False if self.curRating != "1" else True)
+			self.getControl(RATE_CUR_ADVANCED_2).setVisible(False if self.curRating != "2" else True)
+			self.getControl(RATE_CUR_ADVANCED_3).setVisible(False if self.curRating != "3" else True)
+			self.getControl(RATE_CUR_ADVANCED_4).setVisible(False if self.curRating != "4" else True)
+			self.getControl(RATE_CUR_ADVANCED_5).setVisible(False if self.curRating != "5" else True)
+			self.getControl(RATE_CUR_ADVANCED_6).setVisible(False if self.curRating != "6" else True)
+			self.getControl(RATE_CUR_ADVANCED_7).setVisible(False if self.curRating != "7" else True)
+			self.getControl(RATE_CUR_ADVANCED_8).setVisible(False if self.curRating != "8" else True)
+			self.getControl(RATE_CUR_ADVANCED_9).setVisible(False if self.curRating != "9" else True)
+			self.getControl(RATE_CUR_ADVANCED_10).setVisible(False if self.curRating != "10" else True)
 
-    def onAction(self, action):
-        if action.getId() in (ACTION_PARENT_DIRECTORY, ACTION_PREVIOUS_MENU):
-            Debug("[Rating] Closing Dialog")
-            self.close()
+	def onAction(self, action):
+		if action.getId() in (ACTION_PARENT_DIRECTORY, ACTION_PREVIOUS_MENU):
+			Debug("[Rating] Closing Dialog")
+			self.close()
 
 class RateEpisodeDialog(RateDialog):
-    def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
-        super(RateEpisodeDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
-        self.tvdbid = None
-        self.title = None
-        self.year = None
-        self.season = None
-        self.episode = None
+	def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
+		super(RateEpisodeDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
+		self.tvdbid = None
+		self.title = None
+		self.year = None
+		self.season = None
+		self.episode = None
 
-    def initDialog(self, tvdbid, title, year, season, episode, curRating, rating_type):
-        self.tvdbid = tvdbid
-        self.title = title
-        self.year = year
-        self.season = season
-        self.episode = episode
-        super(RateEpisodeDialog, self).initDialog(curRating, rating_type)
+	def initDialog(self, tvdbid, title, year, season, episode, curRating, rating_type):
+		self.tvdbid = tvdbid
+		self.title = title
+		self.year = year
+		self.season = season
+		self.episode = episode
+		super(RateEpisodeDialog, self).initDialog(curRating, rating_type)
 
-    def onInit(self):
-        super(RateEpisodeDialog, self).onInit(__language__(1304).encode("utf-8", "ignore"))
-        self.getControl(RATE_RATE_SHOW_BTN).setLabel(__language__(1305).encode( "utf-8", "ignore" ))
-        self.getControl(RATE_RATE_SHOW_BTN).setVisible(True)
-        self.getControl(RATE_RATE_SHOW_BG).setVisible(True)
+	def onInit(self):
+		super(RateEpisodeDialog, self).onInit(__language__(1304).encode("utf-8", "ignore"))
+		self.getControl(RATE_RATE_SHOW_BTN).setLabel(__language__(1305).encode( "utf-8", "ignore" ))
+		self.getControl(RATE_RATE_SHOW_BTN).setVisible(True)
+		self.getControl(RATE_RATE_SHOW_BG).setVisible(True)
 
-    def onClick(self, controlId):
-        if controlId == RATE_RATE_SHOW_BTN:
-            self.getControl(RATE_RATE_SHOW_BG).setVisible(False)
-            self.getControl(RATE_RATE_SHOW_BTN).setVisible(False)
-            self.setFocus(self.getControl(RATE_SKIP_RATING))
+	def onClick(self, controlId):
+		if controlId == RATE_RATE_SHOW_BTN:
+			self.getControl(RATE_RATE_SHOW_BG).setVisible(False)
+			self.getControl(RATE_RATE_SHOW_BTN).setVisible(False)
+			self.setFocus(self.getControl(RATE_SKIP_RATING))
 
-            if self.ratingType == "advanced":
-                rateShow = RateShowDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
-            else:
-                rateShow = RateShowDialog("rate.xml", __settings__.getAddonInfo('path'))
+			if self.ratingType == "advanced":
+				rateShow = RateShowDialog("rate_advanced.xml", __settings__.getAddonInfo('path'))
+			else:
+				rateShow = RateShowDialog("rate.xml", __settings__.getAddonInfo('path'))
 
-            curRating = utilities.getShowRatingFromTrakt(self.tvdbid, self.title, self.year, self.ratingType)
-            rateShow.initDialog(self.tvdbid, self.title, self.year, curRating, self.ratingType)
-            rateShow.doModal()
-            del rateShow
-        else:
-            method = functools.partial(utilities.rateEpisodeOnTrakt, self.tvdbid, self.title, self.year, self.season, self.episode)
-            super(RateEpisodeDialog, self).onClick(controlId, method)
+			curRating = utilities.getShowRatingFromTrakt(self.tvdbid, self.title, self.year, self.ratingType)
+			rateShow.initDialog(self.tvdbid, self.title, self.year, curRating, self.ratingType)
+			rateShow.doModal()
+			del rateShow
+		else:
+			method = functools.partial(utilities.rateEpisodeOnTrakt, self.tvdbid, self.title, self.year, self.season, self.episode)
+			super(RateEpisodeDialog, self).onClick(controlId, method)
 
 class RateShowDialog(RateDialog):
-    def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
-        super(RateShowDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
-        self.tvdbid = None
-        self.title = None
-        self.year = None
+	def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
+		super(RateShowDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
+		self.tvdbid = None
+		self.title = None
+		self.year = None
 
-    def initDialog(self, tvdbid, title, year, curRating, rating_type):
-        self.tvdbid = tvdbid
-        self.title = title
-        self.year = year
-        super(RateShowDialog, self).initDialog(curRating, rating_type)
+	def initDialog(self, tvdbid, title, year, curRating, rating_type):
+		self.tvdbid = tvdbid
+		self.title = title
+		self.year = year
+		super(RateShowDialog, self).initDialog(curRating, rating_type)
 
-    def onInit(self):
-        super(RateShowDialog, self).onInit(__language__(1306).encode("utf-8", "ignore"))
+	def onInit(self):
+		super(RateShowDialog, self).onInit(__language__(1306).encode("utf-8", "ignore"))
 
-    def onClick(self, controlId):
-        method = functools.partial(utilities.rateShowOnTrakt, self.tvdbid, self.title, self.year)
-        super(RateShowDialog, self).onClick(controlId, method)
+	def onClick(self, controlId):
+		method = functools.partial(utilities.rateShowOnTrakt, self.tvdbid, self.title, self.year)
+		super(RateShowDialog, self).onClick(controlId, method)
 
 class RateMovieDialog(RateDialog):
-    def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
-        super(RateMovieDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
-        self.imdbid = None
-        self.title = None
-        self.year = None
+	def __init__(self, xml, fallback_path=__settings__.getAddonInfo('path'), defaultskinname="Default", forcefallback=False):
+		super(RateMovieDialog, self).__init__(xml, fallback_path, defaultskinname, forcefallback)
+		self.imdbid = None
+		self.title = None
+		self.year = None
 
-    def initDialog(self, imdbid, title, year, curRating, rating_type):
-        self.imdbid = imdbid
-        self.title = title
-        self.year = year
-        super(RateMovieDialog, self).initDialog(curRating, rating_type)
+	def initDialog(self, imdbid, title, year, curRating, rating_type):
+		self.imdbid = imdbid
+		self.title = title
+		self.year = year
+		super(RateMovieDialog, self).initDialog(curRating, rating_type)
 
-    def onInit(self):
-        super(RateMovieDialog, self).onInit(__language__(1303).encode("utf-8", "ignore"))
+	def onInit(self):
+		super(RateMovieDialog, self).onInit(__language__(1303).encode("utf-8", "ignore"))
 
-    def onClick(self, controlId):
-        method = functools.partial(utilities.rateMovieOnTrakt, self.imdbid, self.title, self.year)
-        super(RateMovieDialog, self).onClick(controlId, method)
+	def onClick(self, controlId):
+		method = functools.partial(utilities.rateMovieOnTrakt, self.imdbid, self.title, self.year)
+		super(RateMovieDialog, self).onClick(controlId, method)
 
