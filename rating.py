3b46617fc68ae2c4f750a1222940705f882097da
N3MIS15 <N3MIS15@gmail.com>
2013-02-10 22:03:31 +1100

    Episodes working

--- rating.py
+++ rating.py
@@ -34,31 +34,39 @@ def rateMedia(media_id, media_type):
 	if media_type == 'movie':
 		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetMovieDetails', 'params': {'movieid': media_id, 'properties': ['title', 'imdbnumber', 'year', 'art']}})['moviedetails']
 		trakt_summary = traktJsonRequest('POST', '/movie/summary.json/%%API_KEY%%/' + xbmc_media['imdbnumber'])
-
-	else:
-		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetEpisodeDetails', 'params': {'episodeid': media_id, 'properties': ['title', 'uniqueid', 'tvshowid', 'showtitle', 'episode', 'season']}})['episodedetails']
-		xbmc_show = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetTVShowDetails', 'params': {'tvshowid': xbmc_media['tvshowid'], 'properties': ['art', 'imdbnumber']}})['tvshowdetails']
-		trakt_summary = traktJsonRequest('POST', '/show/episode/summary.json/%%API_KEY%%/'+xbmc_media['imdbnumber']+'/'+xbmc_media['season']+'/'+xbmc_media['episode'])
-
-		xbmc_media['art'] = xbmc_show['art']
-		xbmc_media['year'] = trakt_summary['show']['year']
-
-	if not trakt_summary['rating'] and not trakt_summary['rating_advanced']:
-		rating_type = utilities.traktSettings['viewing']['ratings']['mode']
 		ratings = trakt_summary['ratings']
 
-		xbmc.executebuiltin('Dialog.Close(all, true)')
-		gui = RatingDialog(
-			"RatingDialog.xml",
-			__settings__.getAddonInfo('path'),
-			media_type=media_type,
-			media=xbmc_media,
-			ratings=ratings,
-			rating_type=rating_type
-		)
+		if trakt_summary['rating'] or trakt_summary['rating_advanced']:
+			Debug('[Rating] Movie has been rated')
+			return
 
-		gui.doModal()
-		del gui
+	else:
+		episode = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetEpisodeDetails', 'params': {'episodeid': media_id, 'properties': ['uniqueid', 'tvshowid', 'episode', 'season']}})['episodedetails']
+		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetTVShowDetails', 'params': {'tvshowid': episode['tvshowid'], 'properties': ['art', 'imdbnumber']}})['tvshowdetails']
+		xbmc_media['episode'] = episode
+
+		trakt_summary = traktJsonRequest('POST', '/show/episode/summary.json/%%API_KEY%%/'+str(xbmc_media['imdbnumber'])+'/'+str(xbmc_media['episode']['season'])+'/'+str(xbmc_media['episode']['episode']))
+		xbmc_media['year'] = trakt_summary['show']['year']
+		ratings = trakt_summary['episode']['ratings']
+
+		if trakt_summary['episode']['rating'] or trakt_summary['episode']['rating_advanced']:
+			Debug('[Rating] Episode has been rated')
+			return
+
+	rating_type = utilities.traktSettings['viewing']['ratings']['mode']
+	xbmc.executebuiltin('Dialog.Close(all, true)')
+
+	gui = RatingDialog(
+		"RatingDialog.xml",
+		__settings__.getAddonInfo('path'),
+		media_type=media_type,
+		media=xbmc_media,
+		ratings=ratings,
+		rating_type=rating_type
+	)
+
+	gui.doModal()
+	del gui
 
 
 class RatingDialog(xbmcgui.WindowXMLDialog):
@@ -96,7 +104,7 @@ class RatingDialog(xbmcgui.WindowXMLDialog):
 		if self.media_type == 'movie':
 			self.getControl(10011).setLabel('%s (%s)' % (self.media['title'], self.media['year']))
 		else:
-			self.getControl(10011).setLabel('%s %s' % (self.media['showtitle'], self.media['label']))
+			self.getControl(10011).setLabel('%s - %s' % (self.media['label'], self.media['episode']['label']))
 
 		if self.rating_type == 'simple':
 			self.getControl(10012).setImage(self.poster)
@@ -136,15 +144,15 @@ class RatingDialog(xbmcgui.WindowXMLDialog):
 			data = traktJsonRequest('POST', '/rate/movie/%%API_KEY%%', params, passVersions=True)
 
 		else:
-			params = {'title': self.media['showtitle'], 'year': self.media['year'], 'season': self.media['season'], 'episode': self.media['episode'], 'rating': rating}
+			params = {'title': self.media['label'], 'year': self.media['year'], 'season': self.media['episode']['season'], 'episode': self.media['episode']['episode'], 'rating': rating}
 
-			if self.media['tvshowid'].isdigit():
-				params['tvdb_id'] = self.media['tvshowid']
+			if self.media['imdbnumber'].isdigit():
+				params['tvdb_id'] = self.media['imdbnumber']
 
 			elif self.media['imdbnumber'].startswith('tt'):
-				params['imdb_id'] = self.media['tvshowid']
+				params['imdb_id'] = self.media['imdbnumber']
 
 			data = traktJsonRequest('POST', '/rate/episode/%%API_KEY%%', params, passVersions=True)
 
-		if data:
-			notification(__language__(1201).encode( "utf-8", "ignore" ), __language__(1167).encode( "utf-8", "ignore" )) # Rating submitted successfully
+		if data != None:
+			notification(__language__(1201).encode('utf-8', 'ignore'), __language__(1167).encode('utf-8', 'ignore')) # Rating submitted successfully
