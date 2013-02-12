c481d6e6b0b935943a19fa50e9d8f84c077421b9
N3MIS15 <N3MIS15@gmail.com>
2013-02-12 16:10:54 +1100

    Simplified the ratings dialog

--- rating.py
+++ rating.py
@@ -15,6 +15,36 @@ rate_episodes = __settings__.getSetting("rate_episode")
 rate_each_playlist_item = __settings__.getSetting("rate_each_playlist_item")
 rate_min_view_time = __settings__.getSetting("rate_min_view_time")
 
+buttons = {
+	10030:	'love',
+	10031:	'hate',
+	11030:	'1',
+	11031:	'2',
+	11032:	'3',
+	11033:	'4',
+	11034:	'5',
+	11035:	'6',
+	11036:	'7',
+	11037:	'8',
+	11038:	'9',
+	11039:	'10'
+}
+
+focus_labels = {
+	10030: __language__(1314).encode('utf-8', 'ignore'),
+	10031: __language__(1315).encode('utf-8', 'ignore'),
+	11030: __language__(1315).encode('utf-8', 'ignore'),
+	11031: __language__(1316).encode('utf-8', 'ignore'),
+	11032: __language__(1317).encode('utf-8', 'ignore'),
+	11033: __language__(1318).encode('utf-8', 'ignore'),
+	11034: __language__(1319).encode('utf-8', 'ignore'),
+	11035: __language__(1320).encode('utf-8', 'ignore'),
+	11036: __language__(1321).encode('utf-8', 'ignore'),
+	11037: __language__(1322).encode('utf-8', 'ignore'),
+	11038: __language__(1323).encode('utf-8', 'ignore'),
+	11039: __language__(1314).encode('utf-8', 'ignore')
+}
+
 
 def ratingCheck(current_video, watched_time, total_time, playlist_length):
 	"""Check if a video should be rated and if so launches the rating dialog"""
@@ -29,10 +59,11 @@ def ratingCheck(current_video, watched_time, total_time, playlist_length):
 def rateMedia(media_id, media_type):
 	"""Launches the rating dialog"""
 	if media_id == None:
+		Debug('[Rating] Missing media_id')
 		return
 
 	if media_type == 'movie':
-		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetMovieDetails', 'params': {'movieid': media_id, 'properties': ['title', 'imdbnumber', 'year', 'art']}})['moviedetails']
+		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetMovieDetails', 'params': {'movieid': media_id, 'properties': ['title', 'imdbnumber', 'year']}})['moviedetails']
 		if xbmc_media == None:
 			Debug('[Rating] Failed to retrieve movie data from XBMC')
 			return
@@ -41,8 +72,6 @@ def rateMedia(media_id, media_type):
 		if trakt_summary == None:
 			Debug('[Rating] Failed to retrieve movie data from trakt')
 			return
-
-		ratings = trakt_summary['ratings']
 
 		if trakt_summary['rating'] or trakt_summary['rating_advanced']:
 			Debug('[Rating] Movie has been rated')
@@ -54,7 +83,7 @@ def rateMedia(media_id, media_type):
 			Debug('[Rating] Failed to retrieve episode data from XBMC')
 			return
 
-		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetTVShowDetails', 'params': {'tvshowid': episode['tvshowid'], 'properties': ['art', 'imdbnumber']}})['tvshowdetails']
+		xbmc_media = xbmcJsonRequest({'jsonrpc': '2.0', 'id': 0, 'method': 'VideoLibrary.GetTVShowDetails', 'params': {'tvshowid': episode['tvshowid'], 'properties': ['imdbnumber']}})['tvshowdetails']
 		if xbmc_media == None:
 			Debug('[Rating] Failed to retrieve tvshow data from XBMC')
 			return
@@ -67,7 +96,6 @@ def rateMedia(media_id, media_type):
 			return
 
 		xbmc_media['year'] = trakt_summary['show']['year']
-		ratings = trakt_summary['episode']['ratings']
 
 		if trakt_summary['episode']['rating'] or trakt_summary['episode']['rating_advanced']:
 			Debug('[Rating] Episode has been rated')
@@ -81,7 +109,6 @@ def rateMedia(media_id, media_type):
 		__settings__.getAddonInfo('path'),
 		media_type=media_type,
 		media=xbmc_media,
-		ratings=ratings,
 		rating_type=rating_type
 	)
 
@@ -90,73 +117,37 @@ def rateMedia(media_id, media_type):
 
 
 class RatingDialog(xbmcgui.WindowXMLDialog):
-	def __init__(self, xmlFile, resourcePath, defaultName='Default', forceFallback=False, media_type=None, media=None, ratings=None, rating_type=None):
+	def __init__(self, xmlFile, resourcePath, defaultName='Default', forceFallback=False, media_type=None, media=None, rating_type=None):
 		self.media_type = media_type
 		self.media = media
 		self.rating_type = rating_type
 
-		if 'poster' in media['art']:
-			self.poster = media['art']['poster']
-		else:
-			self.poster = None
-
-		self.loved = str(ratings['loved'])
-		self.hated = str(ratings['hated'])
-		self.loved_percent = str(ratings['percentage'])
-		self.hated_percent = str(100 - ratings['percentage'])
-		self.buttons = {
-			10030:	'love',
-			10031:	'hate',
-			11030:	'1',
-			11031:	'2',
-			11032:	'3',
-			11033:	'4',
-			11034:	'5',
-			11035:	'6',
-			11036:	'7',
-			11037:	'8',
-			11038:	'9',
-			11039:	'10'
-		}
-
-
 	def onInit(self):
-		self.getControl(10111).setVisible(self.rating_type == 'simple')
-		self.getControl(10112).setVisible(self.rating_type == 'advanced')
-
+		self.getControl(10014).setVisible(self.rating_type == 'simple')
+		self.getControl(10015).setVisible(self.rating_type == 'advanced')
 
 		if self.media_type == 'movie':
-			self.getControl(10011).setLabel('%s (%s)' % (self.media['title'], self.media['year']))
+			self.getControl(10012).setLabel('%s (%s)' % (self.media['title'], self.media['year']))
 		else:
-			self.getControl(10011).setLabel('%s - %s' % (self.media['label'], self.media['episode']['label']))
+			self.getControl(10012).setLabel('%s - %s' % (self.media['label'], self.media['episode']['label']))
 
 		if self.rating_type == 'simple':
-			if self.poster:
-				self.getControl(10012).setImage(self.poster)
-
-			self.getControl(10013).setLabel(self.loved_percent+'%')
-			self.getControl(10014).setLabel('%s[CR]%s' % (self.loved, __language__(1313).encode('utf-8', 'ignore')))
-			self.getControl(10015).setLabel(self.hated_percent+'%')
-			self.getControl(10016).setLabel('%s[CR]%s' % (self.hated, __language__(1313).encode('utf-8', 'ignore')))
-
 			self.setFocus(self.getControl(10030)) #Focus Loved Button
-
 		else:
-			if self.poster:
-				self.getControl(11012).setImage(self.poster)
-
-			self.getControl(11013).setLabel(self.loved_percent+'%')
-			self.getControl(11014).setLabel('%s[CR]%s' % (self.loved, __language__(1313).encode('utf-8', 'ignore')))
-			self.getControl(11015).setLabel(self.hated_percent+'%')
-			self.getControl(11016).setLabel('%s[CR]%s' % (self.hated, __language__(1313).encode('utf-8', 'ignore')))
-
 			self.setFocus(self.getControl(11037)) #Focus 8 Button
 
 
 	def onClick(self, controlID):
-		if controlID in self.buttons:
-			self.rateOnTrakt(self.buttons[controlID])
+		if controlID in buttons:
+			self.rateOnTrakt(buttons[controlID])
 			self.close()
+
+
+	def onFocus(self, controlID):
+		if controlID in focus_labels:
+			self.getControl(10013).setLabel(focus_labels[controlID])
+		else:
+			self.getControl(10013).setLabel('')
 
 
 	def rateOnTrakt(self, rating):
