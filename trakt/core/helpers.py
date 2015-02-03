58956c7c5bd2f27288d9af79b02beba9eab72663
Kolja Lampe <razzeee@gmail.com>
2015-02-03 09:36:40 +0100

    Bump trakt.py to c6b1777be3dc5caddb48bc597eb87a842bc079d5

--- trakt/core/helpers.py
+++ trakt/core/helpers.py
@@ -1,4 +1,8 @@
 import arrow
+import functools
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def to_datetime(value):
@@ -13,3 +17,44 @@ def to_datetime(value):
 
     # Return naive datetime object
     return dt.naive
+
+
+def synchronized(f_lock, mode='full'):
+    if mode == 'full':
+        mode = ['acquire', 'release']
+    elif isinstance(mode, (str, unicode)):
+        mode = [mode]
+
+    def wrap(func):
+        @functools.wraps(func)
+        def wrapped(self, *args, **kwargs):
+            lock = f_lock(self)
+
+            def acquire():
+                if 'acquire' not in mode:
+                    return
+
+                lock.acquire()
+
+            def release():
+                if 'release' not in mode:
+                    return
+
+                lock.release()
+
+            # Acquire the lock
+            acquire()
+
+            try:
+                # Execute wrapped function
+                result = func(self, *args, **kwargs)
+            finally:
+                # Release the lock
+                release()
+
+            # Return the result
+            return result
+
+        return wrapped
+
+    return wrap
