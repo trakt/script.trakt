6a9a9f36ff30c7d521ac8583bcbe2fb2a2626f38
Kolja Lampe <razzeee@gmail.com>
2021-03-09 15:44:59 +0100

    Fix tests

--- tests/test_kodiUtilities.py
+++ tests/test_kodiUtilities.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 
+from resources.lib import kodiUtilities
 import pytest
 import mock
 import sys
@@ -11,13 +12,12 @@ xbmcgui_mock = mock.Mock()
 sys.modules['xbmcgui'] = xbmcgui_mock
 xbmcaddon_mock = mock.Mock()
 sys.modules['xbmcaddon'] = xbmcaddon_mock
-from resources.lib import kodiUtilities
 
 
 def test_notification():
-    assert not xbmc_mock.executebuiltin.called
+    assert not xbmcgui_mock.Dialog().notification.called
     kodiUtilities.notification('header', 'message')
-    assert xbmc_mock.executebuiltin.called
+    assert xbmcgui_mock.Dialog().notification.called
 
 
 def test_showSettings():
