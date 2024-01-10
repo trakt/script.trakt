60d3f5437d86fa6d430ceb974a9427ab8a617f26
danielerl <erldaniel@proton.me>
2024-01-10 11:56:25 +0100

    findMediaObject: Change title fallback to elif clause
    This way the media object will not fall back to only matching by title if title and year did not match to anything

--- resources/lib/utilities.py
+++ resources/lib/utilities.py
@@ -110,7 +110,7 @@ def findMediaObject(mediaObjectToMatch, listToSearch, matchByTitleAndYear):
             result = __findInList(
                 listToSearch, title=mediaObjectToMatch['title'], year=mediaObjectToMatch['year'])
         # match only by title, as some items don't have a year on trakt
-        if result is None and 'title' in mediaObjectToMatch:
+        elif result is None and 'title' in mediaObjectToMatch:
             result = __findInList(
                 listToSearch, title=mediaObjectToMatch['title'])
 
